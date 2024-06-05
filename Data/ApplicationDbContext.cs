afe5b2fd1b4bb1b8b4f7ba58be48da950bc0a1ee
Juan Calderon <jcalderongomez@gmail.com>
2024-06-04 22:54:18 -0500

    Agregar conexión a Base de Datos

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models.Entidades;
+
+namespace Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        public DbSet<Producto> Productos {get; set;}
+        public DbSet<Categoria> Categorias {get; set;}
+        //public DbSet<Marca> Marcas {get; set;}
+        
+
+    }
+}
